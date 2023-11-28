dbf5cfbaacce6d735666c912dce3f6cb01ea153b
yusuferens <yusuferenketen@gmail.com>
2023-11-28 18:41:39 +0300

    implemented simple logic for game

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstCard = 4
+let secondCard = 3
+let summary = firstCard + secondCard;
+
+if (summary === 21){
+    console.log("Blackjack ! You won the game ")
+    } else if (summary <21 ){
+        console.log ("Do you want to draw a new card")
+
+    }
+
+else if(summary > 21) {
+    console.log("You failed")}
