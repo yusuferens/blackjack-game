35a6f39f25eb5c13b859aa30bae2da4cc5d57ceb
yusuferens <yusuferenketen@gmail.com>
2023-11-28 21:28:17 +0300

    created summary variable, start function

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-
-let firstCard = 4
-let secondCard = 3
+let firstCard = 4;
+let secondCard = 3;
 let sum = firstCard + secondCard;
-let hasBlackJack = false
-let isAlive = true 
-let message = ""
-let messageEl = document.getElementById("message-el")
-console.log(messageEl)
-function startGame(){
-if (sum <= 20 ) {
-    console.log("Do you want to draw a new car?")
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
 
-}else if ( sum === 21){
-    console.log("You've got Blackjack!")
-    hasBlackJack = true
-    
-} else {
-    console.log ("Bust!")
-    isAlive = false
+function startGame() {
+    sumEl.textContent = "Sum: " + sum;
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "Bust!";
+        isAlive = false;
+    }
+
+    messageEl.textContent = message;
 }
-
-    
-}
-
-messageEl.textContent = message
-
-
-
